ad688f46b72785d0be6a30ca2f1d25639dbecc1d
Fareedah <olaidefaridat@gmail.com>
2026-09-05 15:23:38 +0100

    Fix frontend API base path

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 ﻿import axios from "axios";
 import { storage } from "../utils/storage";
 
-const api = axios.create({baseURL:import.meta.env.VITE_API_URL });
+const apiBaseUrl = (import.meta.env.VITE_API_URL || "").replace(/\/+$/, "");
+const api = axios.create({ baseURL: `${apiBaseUrl}/api` });
 
 api.interceptors.request.use((config) => {
   const token = storage.getToken();
